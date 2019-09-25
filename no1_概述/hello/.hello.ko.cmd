91a51758152c95206f23497a6b5459eaba1360a4
ljrkernel <1510029189@qq.com>
2019-09-25 17:12:05 +0800

    xiufu

--- no1_概述/hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ljr/LearningLinuxKernel/no1_概述/hello/hello.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/ljr/LearningLinuxKernel/no1_概述/hello/hello.ko /home/ljr/LearningLinuxKernel/no1_概述/hello/hello.o /home/ljr/LearningLinuxKernel/no1_概述/hello/hello.mod.o ;  true
